89e785e516a37f86ebbdbfdfc6cb865b14d6ae8b
lakinsm <lakinsm@rams.colostate.edu>
2016-03-13 18:15:26 -0600

    filter_singleton_blast for usearch singletons

--- filter_singleton_blast.py
+++ filter_singleton_blast.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 ## This script filters the output results from BLASTing the HMM singletons against the nt BLAST database.
 ## Matches only pass filter if they meet the inclusion criteria of cd-hit (80% identity at 50% overlap of a given gene).
@@ -14,7 +14,6 @@ import numpy as np
 ## Vars ##
 ##########
 pass_filter = {}
-high_similarity = {}
 identical = {}
 gene_hash = set()
 annodict = {}
@@ -29,13 +28,13 @@ def index_max(values):
 
 def filter_hits(infile, outfile, annotfile, tranfile):
     with open(infile, 'r') as hitfile, open(outfile, 'w') as out, open(tranfile, 'w') as tranout, open(annotfile, 'r') as annot:
-        annotations = annot.read().strip().split('\n')[1:]
-        [annodict.setdefault(x.split(',')[0], []).append(x.split(',')[1:]) for x in annotations]
-        data = hitfile.read().strip().split('\n')[1:]
+        annotations = annot.read().strip().split('\n')
+        [annodict.setdefault(x.split(',')[0], []).append(x.split(',')[1:4]) for x in annotations]
+        data = hitfile.read().strip().split('\n')
         for hit in data:
             info = hit.split('\t')
             gene_id, target_acc, target_id, gene_len, gene_start, gene_stop, target_len, _, _, evalue, pident, \
-                target_title, seq = info
+                _, _, target_title, _, _, _, seq = info
             name = str(target_id)+str(target_acc)
             gene_hash.add(gene_id)
             if (int(gene_stop) - int(gene_start)) < (int(gene_len) / 2):
@@ -43,35 +42,21 @@ def filter_hits(infile, outfile, annotfile, tranfile):
             if int(float(pident)) is 100:
                 identical.setdefault(gene_id, []).append((name, int(gene_stop) - int(gene_start), seq))
                 continue
-            if int(float(pident)) > 99:
-                high_similarity.setdefault(gene_id, []).append((name, int(gene_stop) - int(gene_start), seq))
             pass_filter.setdefault(gene_id, []).append((name, seq))
         for gene in gene_hash:
             if gene not in pass_filter.keys():
-                if gene not in high_similarity.keys():
-                    if gene not in identical.keys():
-                        print gene
-                        continue
-                    zipped = zip(*identical[gene])
-                    max_indices = np.where(np.array(zipped[1]) == max(zipped[1]))[0].tolist()
-                    [pass_filter.setdefault(gene, []).append((y[0], y[2])) for y in [identical[gene][x] for x in max_indices]]
-                else:
-                    zipped = zip(*high_similarity[gene])
-                    max_indices = np.where(np.array(zipped[1]) == max(zipped[1]))[0].tolist()
-                    [pass_filter.setdefault(gene, []).append((y[0], y[2])) for y in [high_similarity[gene][x] for x in max_indices]]
+                if gene not in identical.keys():
+                    print(gene)
+                    continue
+                zipped = zip(*identical[gene])
+                max_indices = np.where(np.array(zipped[1]) == max(zipped[1]))[0].tolist()
+                [pass_filter.setdefault(gene, []).append((y[0], y[2])) for y in [identical[gene][x] for x in max_indices]]
             if gene not in pass_filter.keys():
                 raise Exception('Should not reach this line')
         for key, entry in pass_filter.iteritems():
-            tranout.write(entry[0][0]+'|singleton_addition,'+','.join(annodict[key][0])+'\n')
-            out.write('>'+entry[0][0]+'|singleton_addition\n'+entry[0][1].replace('-', '')+'\n')
-
-
-
-
-
-
-
-
+            for hit in entry:
+                tranout.write(hit[0]+'|singleton_addition,'+','.join(annodict[key][0])+'\n')
+                out.write('>'+hit[0]+'|singleton_addition\n'+hit[1].replace('-','')+'\n')
 
 
 ##############
